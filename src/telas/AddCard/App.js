2d13636739b33b4a5fdc2916706ab65075ff4d23
taylo <101663639+CollyerXTS@users.noreply.github.com>
2022-07-25 21:42:19 -0300

    Tela Add Card

--- src/telas/AddCard/App.js
+++ src/telas/AddCard/App.js
@@ -1,23 +1,297 @@
-import React, { useEffect } from 'react';
-import { View, Text, ScrollView } from 'react-native';
-import Fontisto from 'react-native-vector-icons/Fontisto';
+import React from 'react';
+import { View, Text, ScrollView, TouchableOpacity, Image, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
+
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import styles from './Style';
+
+
 
 export default function AddCard() {
     return (
-        < View style={styles.container}>
-            <Fontisto style={styles.Icon}
-                name="neutral"
-                size={48}
-                color="#ACACAC"
-            />
-            <Text style={styles.texto}>
-                Você ainda não tem nenhum
-                {'\n'}
-                registro diário. Para começar,
-                {'\n'}
-                toque no ícone de adicionar na tela.
-            </Text>
-        </View >
+        <KeyboardAvoidingView style={styles.containerPrincipal}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 0}
+        >
+            <ScrollView style={styles.containerPrincipal}>
+
+                <View>
+                    <TouchableOpacity
+                        style={styles.subContainerDois}
+                    >
+                        <Text style={styles.modalBotao1}>
+                            X
+                        </Text>
+                    </TouchableOpacity>
+                    <Text style={styles.textoUm}>
+                        Como você está?
+                    </Text>
+
+                    <View style={styles.subContainerTres}>
+                        <MaterialCommunityIcons
+                            name='calendar-month-outline'
+                            size={12}
+                        />
+                        <Text
+                            style={styles.textoDois}>
+                            HOJE, 23 DE JANEIRO
+                        </Text>
+
+                        <MaterialCommunityIcons
+                            style={styles.iconDois}
+                            name='clock-time-three-outline'
+                            size={12}
+                        />
+                        <Text style={styles.textoTres}>
+                            08:35
+                        </Text>
+
+                    </View>
+
+                    <View style={styles.subContainerQuatro}>
+                        <TouchableOpacity>
+                            <View style={[styles.containerEllipse, { backgroundColor: '#304FFE' }]}>
+                                <Image
+                                    style={styles.emojiHappy}
+                                    source={require('../../../assets/humores/radiant.png')}
+                                />
+
+                            </View>
+                            <Text
+                                style={styles.textoEmoji}
+                            >
+                                RADIANTE
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <View style={styles.containerEllipse}>
+                                <Image
+                                    style={styles.emojiHappy}
+                                    source={require('../../../assets/humores/radiant.png')}
+                                />
+
+                            </View>
+                            <Text
+                                style={styles.textoEmoji}
+                            >
+                                FELIZ
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <View style={styles.containerEllipse}>
+                                <Image
+                                    style={styles.emojiHappy}
+                                    source={require('../../../assets/humores/radiant.png')}
+                                />
+
+                            </View>
+                            <Text
+                                style={styles.textoEmoji}
+                            >
+                                OK
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <View style={styles.containerEllipse}>
+                                <Image
+                                    style={styles.emojiHappy}
+                                    source={require('../../../assets/humores/radiant.png')}
+                                />
+
+                            </View>
+                            <Text
+                                style={styles.textoEmoji}
+                            >
+                                TRISTE
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <View style={styles.containerEllipse}>
+                                <Image
+                                    style={styles.emojiHappy}
+                                    source={require('../../../assets/humores/radiant.png')}
+                                />
+
+                            </View>
+                            <Text
+                                style={styles.textoEmoji}
+                            >
+                                ACABADO
+                            </Text>
+                        </TouchableOpacity>
+                    </View >
+                    <View style={styles.subContainerCinco}>
+                        <View style={styles.containerInferiorUm}>
+                            <TouchableOpacity>
+                                <View style={styles.containerIconCircle}>
+                                    <MaterialCommunityIcons
+                                        style={styles.ContainerIcons}
+                                        name="sleep"
+                                        size={35}
+                                        color='#000000'
+                                    />
+
+                                </View>
+                                <Text
+                                    style={styles.textoIcon}
+                                >
+                                    descanso
+                                </Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                                <View style={styles.containerIconCircle}>
+                                    <MaterialCommunityIcons
+                                        style={styles.ContainerIcons}
+                                        name="sleep"
+                                        size={35}
+                                        color='#000000'
+                                    />
+
+                                </View>
+                                <Text
+                                    style={styles.textoIcon}
+                                >
+                                    encontro
+                                </Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                                <View style={styles.containerIconCircle}>
+                                    <MaterialCommunityIcons
+                                        style={styles.ContainerIcons}
+                                        name="sleep"
+                                        size={35}
+                                        color='#000000'
+                                    />
+
+                                </View>
+                                <Text
+                                    style={styles.textoIcon}
+                                >
+                                    filmes e séries
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.containerInferiorUm}>
+                            <TouchableOpacity>
+                                <View style={styles.containerIconCircle}>
+                                    <MaterialCommunityIcons
+                                        style={styles.ContainerIcons}
+                                        name="sleep"
+                                        size={35}
+                                        color='#000000'
+                                    />
+
+                                </View>
+                                <Text
+                                    style={styles.textoIcon}
+                                >
+                                    compras
+                                </Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                                <View style={styles.containerIconCircle}>
+                                    <MaterialCommunityIcons
+                                        style={styles.ContainerIcons}
+                                        name="sleep"
+                                        size={35}
+                                        color='#000000'
+                                    />
+
+                                </View>
+                                <Text
+                                    style={styles.textoIcon}
+                                >
+                                    boa refeição
+                                </Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                                <View style={styles.containerIconCircle}>
+                                    <MaterialCommunityIcons
+                                        style={styles.ContainerIcons}
+                                        name="sleep"
+                                        size={35}
+                                        color='#000000'
+                                    />
+
+                                </View>
+                                <Text
+                                    style={styles.textoIcon}
+                                >
+                                    festa
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.containerInferiorUm}>
+                            <TouchableOpacity>
+                                <View style={styles.containerIconCircle}>
+                                    <MaterialCommunityIcons
+                                        style={styles.ContainerIcons}
+                                        name="sleep"
+                                        size={35}
+                                        color='#000000'
+                                    />
+
+                                </View>
+                                <Text
+                                    style={styles.textoIcon}
+                                >
+                                    esporte
+                                </Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                                <View style={styles.containerIconCircle}>
+                                    <MaterialCommunityIcons
+                                        style={styles.ContainerIcons}
+                                        name="sleep"
+                                        size={35}
+                                        color='#000000'
+                                    />
+
+                                </View>
+                                <Text
+                                    style={styles.textoIcon}
+                                >
+                                    cozinhar
+                                </Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                                <View style={styles.containerIconCircle}>
+                                    <MaterialCommunityIcons
+                                        style={styles.ContainerIcons}
+                                        name="sleep"
+                                        size={35}
+                                        color='#000000'
+                                    />
+
+                                </View>
+                                <Text
+                                    style={styles.textoIcon}
+                                >
+                                    jogos
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+
+                    </View>
+                    <View style={styles.subContainerSeis}>
+                        <TextInput style={styles.subContainerTexto}
+                            placeholder='Escreva aqui oque aconteceu hoje...'
+                        >
+
+                        </TextInput>
+                    </View>
+                    <View
+                        style={styles.subContainerSete}
+                    >
+                        <TouchableOpacity
+                            style={styles.containerBotaoConfirmar}
+                        >
+                            <Text style={styles.textoBotaoConfirmar}>
+                                SALVAR
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </ScrollView >
+        </KeyboardAvoidingView>
     );
 };
--- src/telas/AddCard/Style.js
+++ src/telas/AddCard/Style.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-        height: '100%',
-        alignItems: 'center',
-        backgroundColor: '#E5E5E5'
+    containerPrincipal: {
+        flex: 1,
+        backgroundColor: '#FFFFFF'
     },
     Icon: {
         width: 48,
@@ -13,15 +12,153 @@ const styles = StyleSheet.create({
         marginTop: 100,
         color: '#ACACAC'
     },
-    texto: {
-        marginTop: 28,
+    subContainerDois: {
+        width: 36,
+        height: 36,
+        borderRadius: 12,
+        backgroundColor: '#ebeeff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginLeft: 31,
+        marginTop: 32
+    },
+    modalBotao1: {
+        fontSize: 20,
+        color: '#697FFF'
+    },
+    textoUm: {
+        fontSize: 30,
+        color: '#000000',
+        fontFamily: 'SourceSansPro-Bold',
+        alignSelf: 'center',
+        marginTop: 30,
+    },
+    subContainerTres: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontFamily: 'SourceSansPro-Regular',
+        marginTop: 12
+
+    },
+    textoDois: {
+        marginLeft: 4,
+        fontSize: 16
+
+    },
+    iconDois: {
+        marginLeft: 20
+    },
+    textoTres: {
+        marginLeft: 3,
+        fontSize: 16
+    },
+    subContainerQuatro: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        marginTop: 55,
+        marginStart: 30,
+        marginEnd: 21
+
+    },
+    containerEllipse: {
+        width: 52,
+        height: 52,
+        borderRadius: 30,
+        alignItems: 'center',
+        paddingTop: 5,
+        paddingLeft: 1,
+
+
+    },
+    emojiHappy: {
+        width: 43,
+        height: 43
+    },
+    textoEmoji: {
+        marginTop: 3,
+        fontSize: 11,
+        color: '#C801FA',
+        fontFamily: 'SourceSansPro-Bold',
+        alignSelf: 'center',
+    },
+    subContainerCinco: {
+        width: 350,
+        height: 460,
+        borderRadius: 15,
+        alignSelf: 'center',
+        marginTop: 45,
+        backgroundColor: '#FFFFFF',
+        fontSize: 15,
+        borderColor: '#000000',
+        borderWidth: 1,
+    },
+    containerInferiorUm: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        marginTop: 55,
+        marginEnd: 1,
+        marginStart: 0,
+    },
+    containerIconCircle: {
+        width: 59.5,
+        height: 59.5,
+        borderRadius: 30,
+        alignItems: 'center',
+        paddingTop: 12,
+        borderColor: '#000000',
+        borderWidth: 1,
+
+
+    },
+    textoIcon: {
+        color: '#000000',
+        fontFamily: 'SourceSansPro-SemiBold',
+        marginTop: 8,
+        fontSize: 10,
+        alignSelf: 'center'
+
+    },
+    subContainerSeis: {
+        width: 350,
+        height: 100,
+        borderRadius: 15,
+        alignSelf: 'center',
+        marginTop: 20,
+        backgroundColor: '#FFFFFF',
+        fontSize: 15,
+        borderColor: '#000000',
+        borderWidth: 1,
+    },
+    subContainerTexto: {
+        fontFamily: 'SourceSansPro-Regular',
+        fontSize: 18,
+        color: '#969696',
+        marginLeft: 10
+    }
+    ,
+    subContainerSete: {
+        alignItems: 'center',
+        marginBottom: 40,
+    },
+    containerBotaoConfirmar: {
+        width: 350,
+        height: 52,
+        borderRadius: 6,
         justifyContent: 'center',
         alignItems: 'center',
-        fontFamily: 'SourceSansPro-Regular',
-        textAlign: 'center',
-        color: '#ACACAC',
-        fontSize: 16,
-        letterSpacing: 0.3
+        marginTop: 25,
+        backgroundColor: '#304FFE',
+        fontSize: 15,
+
+
+    },
+    textoBotaoConfirmar: {
+        fontFamily: 'SourceSansPro-Bold',
+        color: '#FFFFFF',
+        fontSize: 15,
+
+
     }
 });
 export default styles
